440c9cec138f05e9553f8126ba68bce2419a2d92
Veerabathiran-DM <160101902+Veerabathiran-DM@users.noreply.github.com>
2025-03-27 12:42:24 +0000

    creating the gcs bucket module

---
+++ main.tf
@@ -0,0 +1,26 @@
+module "gcs_bucket" {
+  source = "terraform-google-modules/cloud-storage/google//modules/simple_bucket"
+
+  name          = var.bucket_name 
+  location      = "US" 
+  versioning    = true
+  force_destroy = true 
+
+  lifecycle_rules = [
+    {
+      action = {
+        type = "SetStorageClass"
+        storage_class = "ARCHIVE"
+      }
+      condition = {
+        age = 30 
+        num_newer_versions = 3
+      }
+    },
+  ]
+}
+
+# Output the bucket name
+output "bucket_name" {
+  value = module.gcs_bucket.bucket_name
+}
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+project_id = poc-ad-target-env-413614
+region = us-central1
+bucket_name = trainging-tf-demo-25
